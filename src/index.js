d6d402e94a48d65489376bc926da9c68c4c871f5
陈威全 <damon@Damons-Book.local>
2020-02-05 12:38:12 +0800

    index添加引用模块

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 require("../view/js/global");
-require("../view/js/components/pagination");
-require("../view/js/components/table");
+require("../view/js/components/upload/upload.js");
+require("../view/js/components/alert/alert.js");
+require("../view/js/components/select/select.js");
+require("../view/js/components/pagination/pagination.js");
+require("../view/js/components/table/table.js");
