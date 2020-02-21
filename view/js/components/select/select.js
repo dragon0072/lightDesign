bfa29eb208e7bd793f6fdc850caf6077f29d7ea0
damon <dragon0072@outlook.com>
2020-02-21 14:47:29 +0800

    fix select控件遮罩层在modal控件中，无法正常生效问题

--- view/js/components/select/select.js
+++ view/js/components/select/select.js
@@ -92,14 +92,16 @@
           .querySelector(".light-select-selection")
           .getAttribute(
             "aria-controls"
-          )}" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity:0;z-index:1001;"></div>`
+          )}" style="position:fixed;top:0;left:0;width:${
+          window.innerWidth
+        }px;height:${window.innerHeight}px;opacity:0;z-index:0;"></div>`
       );
       blurDom.addEventListener("click", event => {
         event.stopPropagation();
         _toggleDropDownList(dom);
         event.currentTarget.remove();
       });
-      document.body.appendChild(blurDom);
+      dom.appendChild(blurDom);
       dom.classList.add("light-select-open", "light-select-focused");
       dom
         .querySelector(".light-select-selection")
